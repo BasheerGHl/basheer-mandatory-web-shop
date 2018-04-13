b5bf39c76fc4012b3df4d49128fd497f93ae14ef
basheerghl <basheer.ghlawanji@gmail.com>
2018-04-13 11:43:14 +0200

    inlamning 4

--- inlamning4/script/products.js
+++ inlamning4/script/products.js
@@ -1,18 +1,5 @@
 /* jshint esversion:6 */
-$(()=>{
-$(document).ready(function () {
-    $(".star").hover(function () {
-        $(this).addClass('starred');
-        $(this).prevAll('.star').addClass('starred');
-    }, function () {
-        $(this).removeClass('starred');
-        $(this).prevAll('.star').removeClass('starred');
-    });
-    $('.star').click(function (e) {
-        $(this).addClass('starred-chosen');
-        $(this).prevAll('.star').addClass("starred-chosen");
-    });
-});
+
 
 $(document).ready(function () {
     $("#addComment").click(function () {
@@ -21,12 +8,12 @@ $(document).ready(function () {
 });
 
 
-let send = document.getElementById("send").value;
+let send = document.getElementById("send");
 send.addEventListener("click", showOrder);
 
-function showOrder(e) {
-    e.preventdefault();
-    fetch("http://demo.edument.se/api/reviews", {
+function showOrder() {
+  
+    fetch("http://demo.edument.se/api/orders", {
             method: "POST",
             headers: {
                 'Accept': 'application/json, text/plain, */*',
@@ -36,31 +23,25 @@ function showOrder(e) {
                 FirstName: "basheer",
                 LastName: "ghlawnji",
                 Email: "basheer.ghlawnji@gmail.com",
-                Phone: "12311421",
+                Phone: 12311421,
                 StreetAddress: "lessebo",
-                ZipCode: "12334",
+                ZipCode: 12334,
                 City: "växjö",
                 Comment: "love it ",
-                OrderItem: [{
-                    Name: "Marna Pink Piggy Steamer",
-                    Price: "12.02",
-                    Description: "Silicone-made multipurpose lid can be placed directly over ingredients in a pot to help them cook evenly, can be used to cover food in the microwave, and also can be used as a jar opener. It is very funny to watch the steam come out from its snout!",
-                    Image: "http:\/\/demo.edument.se\/content\/ProductImages\/1.jpg",
-                    Url: "https:\/\/www.amazon.com\/dp\/B007XHTDPG",
-                }],
-                Reviews: [{
-                    Name: "Anymous",
-                    Comment: "good",
-                    "Rating": 5,
-                }],
+                OrderItems : [{
+            Name: "Marna Pink Piggy Steamer",
+            Price: 12.02,
+            Description: "Silicone-made multipurpose lid can be placed directly over ingredients in a pot to help them cook evenly, can be used to cover food in the microwave, and also can be used as a jar opener. It is very funny to watch the steam come out from its snout!",
+            Image: "http:\/\/demo.edument.se\/content\/ProductImages\/1.jpg",
+            Url: "https:\/\/www.amazon.com\/dp\/B007XHTDPG",
+            }],
             })
         })
         .then((res) => res.json())
         .then((data) => console.log(data))
         .catch(() => {
-            console.log("error");
+            (()=> console.log("error"));
         });
 }
 showOrder();
 
-});
